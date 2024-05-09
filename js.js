1823aa4bd39c11d7c858577a37180b57a204f17c
ninhpoal <phuocnv.nz@gmail.com>
2024-05-09 10:35:46 +0700

    ấ
    sá

--- js.js
+++ js.js
@@ -1,21 +1,26 @@
- // Function to get URL parameters
- function getUrlParameter(name) {
+// Function to get URL parameters
+function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
+}
 
-  // Get parameters from URL
-  var mahh = getUrlParameter('mahh');
-  var tenhh = getUrlParameter('tenhh');
-  var dvt = getUrlParameter('dvt');
-   var ncc = getUrlParameter('ncc');
-  // Display parameters on the page
-  document.getElementById('mahh').textContent = mahh;
-  document.getElementById('tenhh').textContent = tenhh;
-  document.getElementById('dvt').textContent = dvt;
-   document.getElementById('ncc').textContent = ncc;
-  window.onload = function() {
+// Get parameters from URL
+var mahh = getUrlParameter('mahh');
+var tenhh = getUrlParameter('tenhh');
+var dvt = getUrlParameter('dvt');
+var ncc = getUrlParameter('ncc');
+// Display parameters on the page
+document.getElementById('mahh').textContent = mahh;
+document.getElementById('tenhh').textContent = tenhh;
+document.getElementById('dvt').textContent = dvt;
+document.getElementById('ncc').textContent = ncc;
+window.onload = function () {
     window.print();
-  };
+};
+
+
+function printPage() {
+    window.print();
+  }
